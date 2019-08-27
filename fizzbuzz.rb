ce2b146e734217b5a3583b21a59d2adae1a13fb6
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-08-27 14:50:34 -0500

    lab complete

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+    result = ''
+    if num % 3 == 0
+        result += 'Fizz'
+    end
+    if num % 5 == 0
+        result += 'Buzz'
+    end
+    result == '' ? nil : result
+end
